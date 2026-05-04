122a137514d43e161399fa31e19cb6de2188dc7a
faydzak <fairuzdzak05@gmail.com>
2026-05-04 21:35:49 +0700

    Case study module 10 DBS

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,5 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
